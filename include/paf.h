007a224f6aed5656de7a4e84e8c8882f5308dad9
Mattias Rönnblom <mattias.ronnblom@ericsson.com>
2024-10-18 07:01:52 +0200

    Update meta data for release 1.1.11
    Update version information in the API documentation and Autoconf in
    preparation for libpaf v1.1.11.
    
    Signed-off-by: Mattias Rönnblom <mattias.ronnblom@ericsson.com>

--- include/paf.h
+++ include/paf.h
@@ -23,7 +23,7 @@ extern "C" {
  *
  * @author Mattias Rönnblom
  * @version 0.1 [API]
- * @version 1.1.10 [Implementation]
+ * @version 1.1.11 [Implementation]
  *
  * @section overview Overview
  *
